4bed4808a330de09c381f2f6369c10466df07ee7
Oscar <oscarlpzluna@gmail.com>
2024-02-16 13:08:20 -0600

    Pruebas en app.py para ver el despliegue de la apliación en local.

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ import plotly_express as px
 
 
 # Se carga el dataset de interés
-data = pd.read_csv('../vehicles_us.csv')
+data = pd.read_csv('vehicles_us.csv')
 
-st.header('Datos recabados sobre los anuncios de automóviles')
+st.header('Datos recabados sobre los anuncios de ventas de automóviles')
+hist_button = st.button('Construir histograma')
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
