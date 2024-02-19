960dfe93322d5f6fdc6b9f21bb6b49ba16ee1e1a
Oscar <oscarlpzluna@gmail.com>
2024-02-19 00:22:25 -0600

    Desarrollo del análisis exploratorio en el notebook, exportar gráficos a la app web en el archivo app.py, y desarrollo del archivo README.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # TT-Proyecto-5
+
+Proyecto 5 del bootcap "Data Analyst" de TripleTen.
+Desarrollado por Óscar López.
+
+En este proyecto se hace el despliegue del panel de control de una aplicación web sencilla
+en un servicio en la nube: Render.
+
+La aplicación web consiste la muestra de diferentes gráficos correspondientes al análisis
+exploratorio de datos realizado sobre un dataset de venta de vehículos('vehicles_us.csv'),
+se incluye el análisis realizado en el archivo 'EDA.ipynb' dentro del directorio 'notebooks'.
+
+Se usaron las siguientes librerias de Python para el desarrollo de este proyecto:
+    -Streamlit: Para la construcción de la aplicación web
+    -Pandas: Para el análisis y manipulación de los dataframes.
+    -Plotly Express: Para la creación de gráficos.
+
+Descripción de los archivos:
+
+- 'config.toml', en el directorio '.streamlit' contiene la información necesaria para configurar
+el servidor donde se desplegará la aplicación.
+- 'app.py' es el scrip en el cual está construida la aplicación web.
+- 'requirements.txt' es un archivo necesario para indicar a Render las librerias necesarias para
+correr el script 'app.py'.
+- 'vehicles_us.csv', conjunto de datos de interés.
+
+
+
--- app.py
+++ app.py
@@ -5,17 +5,58 @@ import plotly_express as px
 
 # Se carga el dataset de interés
 data = pd.read_csv('vehicles_us.csv')
+# Creacion del header de la página
+st.header('Datos recabados sobre los anuncios de ventas de automóviles',
+          divider='rainbow')
+# Creacion del subheader
+st.subheader('Seleccione el tipo de gráfico que desea ver:')
+# Creación de las checkbox con las opciones disponibles.
+hist_box = st.checkbox(
+    'Distribución de los vehículos por año del modelo.')
+disp_box_price = st.checkbox(
+    'Precio del automóvil en función del año del modelo.')
+disp_box_odo = st.checkbox(
+    'Medida del odómetro en función del año del modelo.')
+st.write('En todas las opciones podrá escoger la condición del automóvil que le interese analizar.')
+st.divider()
 
-st.header('Datos recabados sobre los anuncios de ventas de automóviles')
-hist_button = st.button('Construir histograma')
+if hist_box:
+    st.subheader(
+        'Histograma sobre los modelos por año disponibles dependiendo de la condición del automóvil.')
 
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Puede seleccionar la condición del automóvil que desee ver u ocultar.')
 
-    # crear un histograma
-    fig = px.histogram(data, x="odometer")
+    fig = px.histogram(data, x='model_year', color='condition', labels=dict(
+        model_year="Año del modelo", count="Frecuencia", condition="Condición"))
+    fig.update_layout(
+        title='Año del modelo.')
 
-    # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+if disp_box_price:
+    st.subheader('Gráfico de dispersión del precio por año del modelo.')
+
+    st.write('Puede seleccionar la condición del automóvil que desee ver u ocultar.')
+
+    fig = px.scatter(data, x="model_year", y="price", color='condition',
+                     labels=dict(model_year="Año del modelo", price="Precio ($)", condition="Condición"))
+    fig.update_layout(
+        title='Precio del vehículo dependiendo del año del modelo')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_box_odo:
+    st.subheader(
+        'Gráfico de dispersión de la medición del odómetro por año del modelo.')
+
+    st.write('Puede seleccionar la condición del automóvil que desee ver u ocultar.')
+
+    fig = px.scatter(data, x='model_year', y='odometer', color='condition',
+                     labels=dict(model_year="Año del modelo", odometer="Medición del odómetro (Distancia)", condition="Condición"))
+    fig.update_layout(
+        title='Medición del odómetro en función del año del modelo.')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+else:
+    st.write('\n\n\t Ningún gráfico ha sido seleccionado.')
